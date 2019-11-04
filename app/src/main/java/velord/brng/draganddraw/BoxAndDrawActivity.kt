f5f4e51bc9f6e7b19098cd9368cce3cc67043ca2
Velord <33905170+velord@users.noreply.github.com>
2019-11-04 21:52:23 +0300

    Chapter 30: Custom Views and Touch Events is Done !

--- app/src/main/java/velord/brng/draganddraw/BoxAndDrawActivity.kt
+++ app/src/main/java/velord/brng/draganddraw/BoxAndDrawActivity.kt
@@ -1,12 +1,12 @@
 package velord.brng.draganddraw
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class BoxAndDrawActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.box_and_draw_activity)
     }
 }
